a063998c289b261b7222c444154cfa79ffe5371c
sause668 <soulsitescoding@zohomail.com>
2024-11-04 21:23:54 -0500

    fixed mirgation list

--- migrations/versions/20241104_182210_create_watchlist_stocks_table.py
+++ migrations/versions/20241104_182210_create_watchlist_stocks_table.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '88621951fa1e'
-down_revision = '31079fe82ea8'
+down_revision = '001652c77aaa'
 branch_labels = None
 depends_on = None
 
