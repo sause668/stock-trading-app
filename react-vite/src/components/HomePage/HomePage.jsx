9cbe0e7534521c148ac27a395dff5da24aff72e8
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-11-07 17:43:01 -0800

    Basic homepage structure introduced

---
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux";
+import LandingPage from "./LandingPage";
+import ProfilePage from "./ProfilePage";
+
+/** ### Home Page Component
+ * Abstraction layer determining what gets displayed on the homepage whilst retaining modularity. On its own, this page has no content.
+ * @returns A ternary operator which utilizes `sessionUser` to determine whether to display the `ProfilePage` or the `LandingPage`.
+ */
+export default function HomePage() {
+    const sessionUser = useSelector((state) => state.session.user);
+
+    return (<> { sessionUser ? <ProfilePage user={sessionUser} /> : <LandingPage /> } </>)
+}
---
+++ react-vite/src/components/HomePage/LandingPage.jsx
@@ -0,0 +1,11 @@
+/** 
+ * ### Landing Page Component
+ * Displays a page containing *site information* whilst in the **logged out** state.
+ * @returns some
+ */
+export default function LandingPage() {
+    return (<>
+        <h1>Logged Out Home Page</h1>
+        <p>Insert preset propaganda here for why you should make an account with us</p>
+    </>)
+}
