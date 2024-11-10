e0071d7487d8813227ff9f508406ea8f78ed6bd0
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-11-10 12:51:19 -0800

    Abstracted for easier submodule access

---
+++ react-vite/src/components/HomePage/Modules/TransactionModule.jsx
@@ -0,0 +1,14 @@
+/**
+ * ### Transaction Module
+ * Intended only for use within the `ProfilePage` component.
+ * 
+ * Displays relevant user data pertaining to orders they have created.
+ * @param user The user data passed in from the parent.
+ */
+export default function TransactionModule({ user }) {
+    return (<div id="profile-left__transactions">
+        {/* Code here */}
+
+        
+    </div>)
+}
