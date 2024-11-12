c2edef7a41724506997fe47962343448a048003a
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-11-12 10:22:50 -0800

    removed params & crypto references

--- react-vite/src/components/HomePage/Modules/PortfolioModule.jsx
+++ react-vite/src/components/HomePage/Modules/PortfolioModule.jsx
@@ -4,17 +4,11 @@ import { Divider, SingleStock } from "./StockListHelpers";
  * ### Portfolio Module
  * Intended only for use within the `ProfilePage` component.
  * 
- * Displays a `StockList` of every stock & cryptocurrency owned by the current user.
- * @param user The user data passed in from the parent.
+ * Displays a `StockList` of every stock owned by the current user.
  */
-export default function PortfolioModule({ user }) {
+export default function PortfolioModule() {
     return (<div className="profile-stock-list" id="profile-right__portfolio">
-        <h3 className="psl-section-head">Cryptocurrencies</h3>
-        <Divider />
-        {/* Insert: List of currently held cryptocurrencies */}
-        <p></p>
-        <Divider />
-        <h3 className="psl-section-head">Stocks</h3>
+        <h3 className="psl-section-head">Portfolio</h3>
         <Divider />
         {/* Insert: List of currently held stocks */}
         <p></p>
