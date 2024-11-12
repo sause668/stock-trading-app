122baf4abf69dc22125af02fbaaa889a48dc2f97
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-11-12 10:23:21 -0800

    now iterates through owned stocks in state

--- react-vite/src/components/HomePage/Modules/PortfolioModule.jsx
+++ react-vite/src/components/HomePage/Modules/PortfolioModule.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import { Divider, SingleStock } from "./StockListHelpers";
 
 /**
@@ -7,11 +8,14 @@ import { Divider, SingleStock } from "./StockListHelpers";
  * Displays a `StockList` of every stock owned by the current user.
  */
 export default function PortfolioModule() {
+    // Get the current user's list of owned stocks.
+    const stocks = useSelector((state) => state.stock.stocks);
+    
     return (<div className="profile-stock-list" id="profile-right__portfolio">
         <h3 className="psl-section-head">Portfolio</h3>
         <Divider />
-        {/* Insert: List of currently held stocks */}
-        <p></p>
+        {/* Map through the owned stocks array and create a SingleStock tile for each owned stock. */}
+        {stocks?.map((stock) => <SingleStock key={stocks.indexOf(stock)} stock={stock} />)}
         <Divider />
     </div>)
 }
