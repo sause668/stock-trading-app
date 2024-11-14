7d9637594e39c38fa4c3fc57e3c6c4f339e019ec
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-11-14 03:01:29 -0800

    added delete button to watchlist stocks

--- react-vite/src/components/HomePage/Modules/StockListHelpers.jsx
+++ react-vite/src/components/HomePage/Modules/StockListHelpers.jsx
@@ -1,5 +1,6 @@
 /** The below "components" are helpers for code that would otherwise have been repeated a lot in the Stock List
     on the right side of the profile page. Not to be included in the re-exporter for HomePage. */
+import { MdDeleteForever } from "react-icons/md";
 import { TiArrowSortedUp, TiArrowSortedDown } from "react-icons/ti";
 
 /** 
@@ -36,7 +37,13 @@ export function SingleStock({ stock }) {
     })();
 
     return (<div className="psl-stock">
-        <h5>{stock.name}</h5>
+        <div className="psl-stock__info-ctrl">
+            <h5>{stock.name}</h5>
+            {stock.color
+                ? <button className="psl-btn"><MdDeleteForever /></button> 
+                : <></>
+            }
+        </div>
         <div className="psl-stock__trend">
             <p className="psl-stock__val">${stockVal}</p>
             <p className="psl-stock__prc" style={{color: stockCol}}>
