e5818900ca19c3597928c1f697ef629329a473a6
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-11-16 07:10:09 -0800

    removed Orders module (no longer in scope)

--- react-vite/src/components/HomePage/ProfilePage.jsx
+++ react-vite/src/components/HomePage/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { OrderModule, PortfolioModule, TransactionModule, WatchlistModule } from "./Modules";
+import { PortfolioModule, TransactionModule, WatchlistModule } from "./Modules";
 import Transactions from "../Transactions"; 
 
 import { getUserStocks } from "../../redux/stock";
@@ -55,7 +55,6 @@ export default function ProfilePage({ user }) {
             <h1>Investing</h1>
 
             <section id="profile-left">
-                <OrderModule user={user} />
                 <TransactionModule user={user} />
                 <Transactions />
             </section>
@@ -67,62 +66,3 @@ export default function ProfilePage({ user }) {
         </main>
     );
 }
-
-
-// import { useEffect, useState } from "react";
-// import { useDispatch } from "react-redux";
-// import { OrderModule, PortfolioModule, TransactionModule, WatchlistModule } from "./Modules";
-// import Transactions from "../Transactions"; 
-
-// import { getUserStocks } from "../../redux/stock";
-// import { fetchWatchlists } from "../../redux/watchlist";
-// import "./StockList.css";
-
-// /** 
-//  * ### Profile Page Component
-//  * Displays a page containing *user information* whilst in the **logged in** state.
-//  * 
-//  * The page is split into two halves. The left half manages the user's Orders and Transactions.
-//  * The right half manages the user's Portfolio (owned stock list) and existing Watchlists.
-//  */
-// export default function ProfilePage({ user }) {
-//     const dispatch = useDispatch();
-//     const [portfolio, setPortfolio] = useState(null);
-
-//     useEffect(() => {
-//         if (user) {
-//             dispatch(getUserStocks());
-//             dispatch(fetchWatchlists());
-//             fetchPortfolio(); // Fetch portfolio data
-//         } else {
-//             console.error("Profile page loaded without a logged-in user!");
-//         }
-//     }, [dispatch, user]);
-
-//     const fetchPortfolio = async () => {
-//         const res = await fetch("/api/portfolio");
-//         if (res.ok) {
-//             const data = await res.json();
-//             setPortfolio(data);
-//         } else {
-//             console.error("Failed to fetch portfolio");
-//         }
-//     };
-
-//     return (
-//         <main id="profile-main">
-//             <h1>Investing</h1>
-
-//             <section id="profile-left">
-//                 <OrderModule user={user} />
-//                 <TransactionModule user={user} />
-//                 <Transactions />
-//             </section>
-
-//             <section id="profile-right">
-//                 <PortfolioModule portfolio={portfolio} fetchPortfolio={fetchPortfolio} />
-//                 <WatchlistModule user={user} />
-//             </section>
-//         </main>
-//     );
-// }
