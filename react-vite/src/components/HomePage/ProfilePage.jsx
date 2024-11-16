5d86ff30c101a6b50f73b04e5c96004de3c87285
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-11-16 11:15:11 -0800

    documentation corrections

--- react-vite/src/components/HomePage/ProfilePage.jsx
+++ react-vite/src/components/HomePage/ProfilePage.jsx
@@ -10,6 +10,11 @@ import "./StockList.css";
 /** 
  * ### Profile Page Component
  * Displays a page containing *user information* whilst in the **logged in** state.
+ * 
+ * The Profile Page is comprised of three modules split into two sides of the page. 
+ * Two modules make up the left side of the page, and the third occupies the right 
+ * side. The end goal is for this page to be the all-in-one information center that
+ * keeps the user up-to-date on all of their activity within the service.
  */
 export default function ProfilePage({ user }) {
     const dispatch = useDispatch();
