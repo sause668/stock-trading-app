80043f386cd22808bdacdca3cd77a0178eb2d35a
brodrickbaker <brodrickrbaker@gmail.com>
2024-11-17 15:32:45 -0500

    fixed footer address

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,7 +1,6 @@
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import StockSearch from "./StockSearch";
-import { SiRobinhood } from "react-icons/si";
 import "./Navigation.css";
 
 function Navigation() {
